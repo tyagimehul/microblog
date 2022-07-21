460b2b101e7f66537c566f96887345445211bdb6
tyagimehul <tyagi.mehul1@gmail.com>
2022-07-21 09:25:46 +0530

    Added User profile page

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -10,22 +10,32 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("{} is already taken, please use a different username.".format(username.data))
-    
+            raise ValidationError(
+                "{} is already taken, please use a different username.".format(
+                    username.data
+                )
+            )
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Please use a different email address.")
-    
-    
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from distutils.log import Log
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_required, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route("/")
@@ -12,8 +20,8 @@ from werkzeug.urls import url_parse
 @login_required
 def index():
     posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {"author": current_user, "body": "Beautiful day in Portland!"},
+        {"author": current_user, "body": "The Avengers movie was so cool!"},
     ]
     return render_template("index.html", title="Home Page", posts=posts)
 
@@ -21,7 +29,9 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        flash("You are already logged in as {}".format(current_user.username)) # can remove
+        flash(
+            "You are already logged in as {}".format(current_user.username)
+        )  # can remove
         return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
@@ -30,9 +40,9 @@ def login():
             flash("Invalid user or password!")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            flash("Successfully logged in as {}".format(user.username)) #can remove
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            flash("Successfully logged in as {}".format(user.username))  # can remove
             return redirect(url_for("index"))
         return redirect(next_page)
     return render_template("login.html", form=form, title="Sign In")
@@ -43,10 +53,13 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        flash("You are already logged in as {}".format(current_user.username)) # can remove
+        flash(
+            "You are already logged in as {}".format(current_user.username)
+        )  # can remove
         return redirect(url_for("index"))
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -56,13 +69,31 @@ def register():
         db.session.commit()
         flash("Congratulations on successful registration!")
         return redirect(url_for("login"))
-    return render_template("register.html",form=form,title="Register")
+    return render_template("register.html", form=form, title="Register")
+
 
 @app.route("/user/<username>")
+@login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'author': user, 'body': 'Test Post 1'},
-        {'author': user, 'body': 'Test Post 2'}
+        {"author": user, "body": "Test Post 1"},
+        {"author": user, "body": "Test Post 2"},
     ]
     return render_template("user.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+        return redirect(url_for("user", username=current_user.username))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
