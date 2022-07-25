3c1b68bd188fba898881bb25c3adf6850f36f7f2
tyagimehul <tyagi.mehul1@gmail.com>
2022-07-25 20:30:01 +0530

    Date and Time fix using flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -16,6 +17,7 @@ mail = Mail(app)
 login = LoginManager(app)
 login.login_view = "login"
 bootstrap = Bootstrap5(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
