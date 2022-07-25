3249c6af5b2780c074ed88ac9eaeab3f98adc1fd
tyagimehul <tyagi.mehul1@gmail.com>
2022-07-25 19:26:46 +0530

    Added Bootstarpfor frontend

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap5
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 login = LoginManager(app)
 login.login_view = "login"
+bootstrap = Bootstrap5(app)
 
 from app import routes, models, errors
 
